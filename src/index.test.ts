e97142e5374d14e6c9a52e57e37dc325823e9535
Alloys Mila <mofax@users.noreply.github.com>
2022-09-06 10:29:14 +0300

    chore: we love tabs

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,17 @@
 import ksuid, { parse } from "./index";
 
 describe("ksuid tests", () => {
-  it("generates valid ids", () => {
-    let id = ksuid();
-    expect(typeof id).toBe("string");
-    expect(id.length).toBe(26);
-  });
+	it("generates valid ids", () => {
+		let id = ksuid();
+		expect(typeof id).toBe("string");
+		expect(id.length).toBe(26);
+	});
 
-  it("generates ids with specific dates", () => {
-    let date = new Date();
-    let id = ksuid(date);
-    let obj = parse(id);
-    let objTime = new Date(obj.time);
-    expect(date.getTime()).toBe(objTime.getTime());
-  });
-
+	it("generates ids with specific dates", () => {
+		let date = new Date();
+		let id = ksuid(date);
+		let obj = parse(id);
+		let objTime = new Date(obj.time);
+		expect(date.getTime()).toBe(objTime.getTime());
+	});
 });
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { randomBytes } from "crypto";
 import * as baseX from "base-x";
 
 const baseAlpha =
-  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const base62 = baseX(baseAlpha);
 
 const ID_LENGTH = 20;
@@ -10,48 +10,50 @@ const TIME_LENGTH = 7;
 const PAYLOAD_LENGTH = 13;
 
 function getTime(date?: Date) {
-  let _date = date === undefined ? new Date() : date;
-  let timeStr = `0${_date.getTime()}`;
-  if (timeStr.length !== TIME_LENGTH * 2)
-    throw new Error(
-      `time length should be ${TIME_LENGTH * 2} generated ${timeStr.length}`
-    );
-  return Buffer.from(timeStr, "hex");
+	let _date = date === undefined ? new Date() : date;
+	let timeStr = `0${_date.getTime()}`;
+	if (timeStr.length !== TIME_LENGTH * 2)
+		throw new Error(
+			`time length should be ${TIME_LENGTH * 2} generated ${
+				timeStr.length
+			}`
+		);
+	return Buffer.from(timeStr, "hex");
 }
 
 function getPayload() {
-  let bytes = randomBytes(PAYLOAD_LENGTH);
-  return bytes;
+	let bytes = randomBytes(PAYLOAD_LENGTH);
+	return bytes;
 }
 
 export default function mKsuid(date?: Date) {
-  let time = getTime(date);
-  let payload = getPayload();
-  let id = Buffer.concat([time, payload]);
-  return base62.encode(id);
+	let time = getTime(date);
+	let payload = getPayload();
+	let id = Buffer.concat([time, payload]);
+	return base62.encode(id);
 }
 
 function parse(str: string) {
-  let id = base62.decode(str);
-  if (id.length !== ID_LENGTH) throw new Error(`invalid mksuid`);
-  let timeLength = TIME_LENGTH;
-  let payloadLength = PAYLOAD_LENGTH;
-  let time = Buffer.allocUnsafe(timeLength);
-  let payload = Buffer.allocUnsafe(payloadLength);
-  for (let i = 0; i < id.length; i++) {
-    if (i < timeLength) {
-      time[i] = id[i];
-    } else {
-      payload[i - timeLength] = id[i];
-    }
-  }
+	let id = base62.decode(str);
+	if (id.length !== ID_LENGTH) throw new Error(`invalid mksuid`);
+	let timeLength = TIME_LENGTH;
+	let payloadLength = PAYLOAD_LENGTH;
+	let time = Buffer.allocUnsafe(timeLength);
+	let payload = Buffer.allocUnsafe(payloadLength);
+	for (let i = 0; i < id.length; i++) {
+		if (i < timeLength) {
+			time[i] = id[i];
+		} else {
+			payload[i - timeLength] = id[i];
+		}
+	}
 
-  let timeStr = time.toString("hex");
+	let timeStr = time.toString("hex");
 
-  return {
-    time: new Date(parseInt(timeStr)),
-    payload,
-  };
+	return {
+		time: new Date(parseInt(timeStr)),
+		payload,
+	};
 }
 
 export { parse };
